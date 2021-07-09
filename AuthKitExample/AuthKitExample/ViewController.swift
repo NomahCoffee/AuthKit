e044a7f7116becc71d9db999d2f0655161bfcced
Caleb Rudnicki <calebrudnicki@calebs-mbp.myfiosgateway.com>
2021-07-09 16:50:14 -0400

    Reorganized the structure of AuthKit

---
+++ AuthKitExample/AuthKitExample/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  AuthKitExample
+//
+//  Created by Caleb Rudnicki on 7/9/21.
+//
+
+import UIKit
+import AuthKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    let logoutButtonNoChange: UIButton = {
+        let logoutButtonNoChange = UIButton()
+        logoutButtonNoChange.setTitle("Logout (existing AuthKit options)", for: .normal)
+        logoutButtonNoChange.setTitleColor(.label, for: .normal)
+        logoutButtonNoChange.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+        return logoutButtonNoChange
+    }()
+    
+    let logoutButtonBothLoginAndLogout: UIButton = {
+        let logoutButtonBothLoginAndLogout = UIButton()
+        logoutButtonBothLoginAndLogout.setTitle("Logout (show login and signup)", for: .normal)
+        logoutButtonBothLoginAndLogout.setTitleColor(.label, for: .normal)
+        logoutButtonBothLoginAndLogout.addTarget(self, action: #selector(logoutButtonBothLoginAndLogoutTapped), for: .touchUpInside)
+        return logoutButtonBothLoginAndLogout
+    }()
+    
+    let logoutButtonToLoginOnly: UIButton = {
+        let logoutButtonToLoginOnly = UIButton()
+        logoutButtonToLoginOnly.setTitle("Logout (show login only)", for: .normal)
+        logoutButtonToLoginOnly.setTitleColor(.label, for: .normal)
+        logoutButtonToLoginOnly.addTarget(self, action: #selector(logoutButtonToLoginOnlyTapped), for: .touchUpInside)
+        return logoutButtonToLoginOnly
+    }()
+    
+    let logoutButtonToSignupOnly: UIButton = {
+        let logoutButtonToSignupOnly = UIButton()
+        logoutButtonToSignupOnly.setTitle("Logout (show signup only)", for: .normal)
+        logoutButtonToSignupOnly.setTitleColor(.label, for: .normal)
+        logoutButtonToSignupOnly.addTarget(self, action: #selector(logoutButtonToSignupOnlyTapped), for: .touchUpInside)
+        return logoutButtonToSignupOnly
+    }()
+    
+    let stack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 8
+        return stack
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(stack)
+        
+        stack.addArrangedSubview(logoutButtonNoChange)
+        stack.addArrangedSubview(logoutButtonBothLoginAndLogout)
+        stack.addArrangedSubview(logoutButtonToLoginOnly)
+        stack.addArrangedSubview(logoutButtonToSignupOnly)
+        
+        stack.snp.makeConstraints { make in
+            make.edges.centerY.equalToSuperview()
+        }
+    }
+    
+    @objc private func logoutButtonTapped() {
+        AuthKitManager.shared.logout(from: self)
+    }
+    
+    @objc private func logoutButtonBothLoginAndLogoutTapped() {
+        AuthKitManager.shared.membershipOption = .loginAndSignup
+        AuthKitManager.shared.logout(from: self)
+    }
+    
+    @objc private func logoutButtonToLoginOnlyTapped() {
+        AuthKitManager.shared.membershipOption = .loginOnly
+        AuthKitManager.shared.logout(from: self)
+    }
+    
+    @objc private func logoutButtonToSignupOnlyTapped() {
+        AuthKitManager.shared.membershipOption = .signupOnly
+        AuthKitManager.shared.logout(from: self)
+    }
+
+}
+
---
+++ AuthKitExample/Pods/Target Support Files/Pods-AuthKitExample/Pods-AuthKitExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AuthKitExample : NSObject
+@end
+@implementation PodsDummy_Pods_AuthKitExample
+@end
